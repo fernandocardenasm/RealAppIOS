81704b0b14466c083f67232ef75bd5b0fede6640
Fernando <fernando@Raffaelles-MBP.fritz.box>
2018-02-06 20:15:08 +0100

    Add Toast

--- RealApp/SeedData.swift
+++ RealApp/SeedData.swift
@@ -19,23 +19,23 @@ class SeedData {
         person1.imageProfile = "claudia_p"
         person1.imageSelf = "claudia_s"
         person1.imageFriend = "claudia_f"
-        person1.listFriends = ["0"]
+        person1.listFriends = ["4", "5", "6"]
         
         let person2 = Person()
         person2.userId = "2"
-        person2.userName = "Mark"
-        person2.imageProfile = "mark_p"
-        person2.imageSelf = "mark_s"
-        person2.imageFriend = "mark_f"
-        person2.listFriends = ["3"]
+        person2.userName = "Razan"
+        person2.imageProfile = "razan_p"
+        person2.imageSelf = "razan_s"
+        person2.imageFriend = "razan_s"
+        person2.listFriends = ["0"]
         
         let person3 = Person()
         person3.userId = "3"
-        person3.userName = "Gabriela"
-        person3.imageProfile = "gabriela_p"
-        person3.imageSelf = "gabriela_s"
-        person3.imageFriend = "gabriela_f"
-        person3.listFriends = ["2", "4"]
+        person3.userName = "Mihaela"
+        person3.imageProfile = "mihaela_p"
+        person3.imageSelf = "mihaela_s"
+        person3.imageFriend = "mihaela_s"
+        person3.listFriends = ["0"]
         
         let person4 = Person()
         person4.userId = "4"
@@ -43,23 +43,23 @@ class SeedData {
         person4.imageProfile = "emilio_p"
         person4.imageSelf = "emilio_s"
         person4.imageFriend = "emilio_f"
-        person4.listFriends = ["3"]
+        person4.listFriends = ["1", "5", "6"]
         
         let person5 = Person()
         person5.userId = "5"
-        person5.userName = "Djanina"
-        person5.imageProfile = "dj_p"
-        person5.imageSelf = "dj_s"
-        person5.imageFriend = "dj_s"
-        person5.listFriends = ["0"]
+        person5.userName = "Azusena"
+        person5.imageProfile = "azu_p"
+        person5.imageSelf = "azu_s"
+        person5.imageFriend = "azu_f"
+        person5.listFriends = ["1", "4", "6"]
         
         let person6 = Person()
         person6.userId = "6"
-        person6.userName = "Rana"
-        person6.imageProfile = "rana_p"
-        person6.imageSelf = "rana_s"
-        person6.imageFriend = "rana_s"
-        person6.listFriends = ["0"]
+        person6.userName = "Sandra"
+        person6.imageProfile = "sandra_p"
+        person6.imageSelf = "sandra_s"
+        person6.imageFriend = "sandra_f"
+        person6.listFriends = ["1", "4", "5"]
         
         
         return [person1, person2, person3, person4, person5, person6]
